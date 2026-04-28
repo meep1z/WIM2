559cb34f6f17eb210fa84977f9104c88d7268272
meep1z <aristarhgavrilenko@gmail.com>
2026-04-28 17:25:40 +0200

    commit 2 - 28.04

---
+++ WIM/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WIM
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
